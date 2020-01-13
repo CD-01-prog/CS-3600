a693954bccf46116d23a48cd14d5bbe686a52872
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-01-13 13:09:17 -0700

    Update Prog2.cpp
    my god the horror

--- Prog2.cpp
+++ Prog2.cpp
@@ -36,43 +36,63 @@ double gDY = 0.07;
 double cx1 = 50;
 double cy1 = 50;
 double cs1 = 20;
-double cv1 = 1;
+double cvx1 = 50;
+double cvy1 = 50;
+
 double cx2 = 500;
 double cy2 = 505;
-double cs2 = 20;
-double cv2 = 1;
+double cs2 = 10;
+double cvx2 = 5;
+double cvy2 = 1;
+
 double cx3 = 650;
 double cy3 = 650;
-double cs3 = 20;
-double cv3 = 1;
+double cs3 = 40;
+double cvx3 = 1;
+double cvy3 = 5;
+
 double cx4 = 300;
 double cy4 = 350;
-double cs4 = 20;
-double cv4 = 1;
+double cs4 = 24;
+double cvx4 = 0.89;
+double cvy4 = 1;
+
 double cx5 = 225;
 double cy5 = 40;
-double cs5 = 20;
-double cv5 = 1;
+double cs5 = 36;
+double cvx5 = 1;
+double cvy5 = 0.89;
+
 double cx6 = 195;
 double cy6 = 525;
-double cs6 = 20;
-double cv6 = 1;
+double cs6 = 42;
+double cvx6 = 10;
+double cvy6 = 5;
+
 double cx7 = 590;
 double cy7 = 75;
-double cs7 = 20;
-double cv7 = 1;
+double cs7 = 15;
+double cvx7 = 7;
+double cvy7 = 5;
+
 double cx8 = 175;
 double cy8 = 30;
-double cs8 = 20;
-double cv8 = 1;
+double cs8 = 9;
+double cvx8 = 4;
+double cvy8 = 9;
+
 double cx9 = 30;
 double cy9 = 425;
-double cs9 = 20;
-double cv9 = 1;
+double cs9 = 30;
+double cvx9 = 4;
+double cvy9 = 4;
+
 double cx10 = 180;
 double cy10 = 180;
-double cs10 = 20;
-double cv10 = 1;
+double cs10 = 28;
+double cvx10 = 1;
+double cvy10 = 1;
+
 
 //
 // Functions that draw basic primitives
@@ -152,83 +172,144 @@ void display(void)
 
     // Test lines that draw all three shapes and some text.
     // Delete these when you get your code working.
-    glColor3d(0,0,1);
-    DrawRectangle(gX, gY, gX+ size, gY+ size);
+    //glColor3d(0,0,1);
+    //DrawRectangle(gX, gY, gX+ size, gY+ size);
 //should be able to make this just a loop with random values seems i can also make a class that holds all of this
     
     
     glColor3d(0,0,1);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx1 + cvx1 + cs1 > screen_x)
+        cvx1 = -cvx1;
+    if (cx1 + cvx1 < 0+cs1)
+        cvx1 = -cvx1;
+    cx1 += cvx1;
+    if (cy1 + cvy1 + cs1 > screen_y)
+        cvy1 = -cvy1;
+    if (cy1 + cvy1 < 0 + cs1)
+        cvy1 = -cvy1;
+    cy1 += cvy1;
     DrawCircle(cx1,cy1,cs1);
     
     glColor3d(0.2,0.7,0.3);
-   out_of_bounds_x(cx1, cv1);
-   out_of_bounds_y(cy1, cv1);
+   if (cx2 + cvx2 + cs2 > screen_x)
+       cvx2 = -cvx2;
+   if (cx2 + cvx2 < 0 + cs2)
+       cvx2 = -cvx2;
+   cx2 += cvx2;
+   if (cy2 + cvy2 + cs2 > screen_y)
+       cvy2 = -cvy2;
+   if (cy2 + cvy2 < 0 + cs2)
+       cvy2 = -cvy2;
+   cy2 += cvy2;
     DrawCircle(cx2,cy2,cs2);
     
     glColor3d(0,0,0);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx3 + cvx3 + cs3 > screen_x)
+        cvx3 = -cvx3;
+    if (cx3 + cvx3 < 0 + cs3)
+        cvx3 = -cvx3;
+    cx3 += cvx3;
+    if (cy3 + cvy3 + cs3 > screen_y)
+        cvy3 = -cvy3;
+    if (cy3 + cvy3 < 0 + cs3)
+        cvy3 = -cvy3;
+    cy3 += cvy3;
     DrawCircle(cx3,cy3,cs3);
     
     glColor3d(0.5,0.1,1);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx4 + cvx4 + cs4 > screen_x)
+        cvx4 = -cvx4;
+    if (cx4 + cvx4 < 0 + cs4)
+        cvx4 = -cvx4;
+    cx4 += cvx4;
+    if (cy4 + cvy4 + cs4 > screen_y)
+        cvy4 = -cvy4;
+    if (cy4 + cvy4 < 0 + cs4)
+        cvy4 = -cvy4;
+    cy4 += cvy4;
     DrawCircle(cx4,cy4,cs4);
     
     glColor3d(0.8,0.2,1);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx5 + cvx5 + cs5 > screen_x)
+        cvx5 = -cvx5;
+    if (cx5 + cvx5 < 0 + cs5)
+        cvx5 = -cvx5;
+    cx5 += cvx5;
+    if (cy5 + cvy5 + cs5 > screen_y)
+        cvy5 = -cvy5;
+    if (cy5 + cvy5 < 0 + cs5)
+        cvy5 = -cvy5;
+    cy5 += cvy5;
     DrawCircle(cx5,cy5,cs5);
     
     glColor3d(0,0,5);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx6 + cvx6 + cs6 > screen_x)
+        cvx6 = -cvx6;
+    if (cx6 + cvx6 < 0 + cs6)
+        cvx6 = -cvx6;
+    cx6 += cvx6;
+    if (cy6 + cvy6 + cs6 > screen_y)
+        cvy6 = -cvy6;
+    if (cy6 + cvy6 < 0 + cs6)
+        cvy6 = -cvy6;
+    cy6 += cvy6;
     DrawCircle(cx6,cy6,cs6);
     
     glColor3d(0.9,0.77,0.2);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx7 + cvx7 + cs7 > screen_x)
+        cvx7 = -cvx7;
+    if (cx7 + cvx7 < 0 + cs7)
+        cvx7 = -cvx7;
+    cx7 += cvx7;
+    if (cy7 + cvy7 + cs7 > screen_y)
+        cvy7 = -cvy7;
+    if (cy7 + cvy7 < 0 + cs7)
+        cvy7 = -cvy7;
+    cy7 += cvy7;
     DrawCircle(cx7,cy7,cs7);
     
     glColor3d(0,0,0);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx8 + cvx8 + cs8 > screen_x)
+        cvx8 = -cvx8;
+    if (cx8 + cvx8 < 0 + cs8)
+        cvx8 = -cvx8;
+    cx8 += cvx8;
+    if (cy8 + cvy8 + cs8 > screen_y)
+        cvy8 = -cvy8;
+    if (cy8 + cvy8 < 0 + cs8)
+        cvy8 = -cvy8;
+    cy8 += cvy8;
     DrawCircle(cx8,cy8,cs8);
     
     glColor3d(1,0,1);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx9 + cvx9 + cs9 > screen_x)
+        cvx9 = -cvx9;
+    if (cx9 + cvx9 < 0 + cs9)
+        cvx9 = -cvx9;
+    cx9 += cvx9;
+    if (cy9 + cvy9 + cs9 > screen_y)
+        cvy9 = -cvy9;
+    if (cy9 + cvy9 < 0 + cs9)
+        cvy9 = -cvy9;
+    cy9 += cvy9;
     DrawCircle(cx9,cy9,cs9);
     
     glColor3d(0,1,1);
-    out_of_bounds_x(cx1, cv1);
-    out_of_bounds_y(cy1, cv1);
+    if (cx10 + cvx10 + cs10 > screen_x)
+        cvx10 = -cvx10;
+    if (cx10 + cvx10 < 0 + cs10)
+        cvx10 = -cvx10;
+    cx10 += cvx10;
+    if (cy10 + cvy10 + cs10 > screen_y)
+        cvy10 = -cvy10;
+    if (cy10 + cvy10 < 0 + cs10)
+        cvy10 = -cvy10;
+    cy10 += cvy10;
     DrawCircle(cx10,cy10,cs10);
-    
     
 
     glutPostRedisplay();
     glutSwapBuffers();
-}
-double out_of_bounds_x(double gX, double gDX){
-    double size = 50;
-    if (gX + gDX + size > screen_x)
-        gDX = -gDX;
-    if (gX + gDX < 0)
-        gDX = -gDX;
-    gX += gDX;
-    return gX;
-}
-double out_of_bounds_y(double gY, double gDY){
-    double size = 50;
-    if (gY + gDY + size > screen_y)
-        gDY = -gDY;
-    if (gY + gDY < 0)
-        gDY = -gDY;
-    gY += gDY;
-    return gY;
 }
 
 // This callback function gets called by the Glut
@@ -327,3 +408,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
