788715f79fe64ef8fa073e74e368ccd5f7d87141
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-02-20 15:20:23 -0700

    Update rat.h

--- P5/rat.h
+++ P5/rat.h
@@ -1,1 +1,29 @@
+//
+//  Rat.hpp
+//  Prog5
+//
+//  Created by Caleb J. Delaney on 2/20/20.
+//  Copyright © 2020 Caleb J. Delaney. All rights reserved.
+//
 
+#ifndef Rat_hpp
+#define Rat_hpp
+
+#include <stdio.h>
+class Rat
+{
+public:
+    Rat();
+    void SpinLeft();
+    void SpinRight();
+    void ScurryForward();
+    void ScurryBackward();
+    void Draw();
+    void RatView();
+private:
+    double x;
+    double y;
+    double degrees;
+};
+
+#endif /* Rat_hpp */
