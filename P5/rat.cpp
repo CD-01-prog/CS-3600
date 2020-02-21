86c1025bd4f02cbb2aceb0e7d1d75505d6abd94d
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-02-21 12:19:05 -0700

    Update rat.cpp

--- P5/rat.cpp
+++ P5/rat.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "Rat.hpp"
+#include "maze.h"
 #include <cmath>
 #include <GLUT/glut.h>
 
@@ -14,37 +15,30 @@ Rat::Rat(){
     x = 2.5;
     y = .5;
     degrees = 90.;
+    dx = std::cos(degrees * 3.1415926 / 180.);
+    dy = std::sin(degrees * 3.1415926 / 180.);
+    Speed = .001;
+    size = .2;
 }
 
 void Rat::SpinLeft(){
     degrees += .05 * 30;
+    dx = std::cos(degrees * 3.1415926 / 180.);
+    dy = std::sin(degrees * 3.1415926 / 180.);
+    
 }
 void Rat::SpinRight(){
     degrees -= .05 * 30;
+    dx = std::cos(degrees * 3.1415926 / 180.);
+    dy = std::sin(degrees * 3.1415926 / 180.);
 }
 void Rat::ScurryForward(){
-    double radians = degrees * 3.1415926 / 180.;
-    double dx = std::cos(radians);
-    double dy = std::sin(radians);
-    double SPEED = .001;
-    //        if (pMaze->IsSafe(x + dx * SPEED, y + dy * SPEED, fat))
-    {
-        x += dx * SPEED * 30;
-        y += dy * SPEED * 30;
-    }
-    //        else if
-
-    //        else if
+        x += dx * Speed * 30;
+        y += dy * Speed * 30;
 }
 void Rat::ScurryBackward(){
-    double radians = degrees * 3.1415926 / 180.;
-    double dx = std::cos(radians);
-    double dy = std::sin(radians);
-    double SPEED = .001;
-    {
-        x -= dx * SPEED * 30;
-        y -= dy * SPEED * 30;
-    }
+    x -= dx * Speed * 30;
+    y -= dy * Speed * 30;
 }
 void Rat::Draw(){
     glPushMatrix();
@@ -53,7 +47,7 @@ void Rat::Draw(){
     glScaled(.5, .5, 1.0);
     {
         glBegin(GL_TRIANGLES);
-        glVertex2d(.5,0);
+        glVertex2d(.3,0);
         glVertex2d(-.3,.2);
         glVertex2d(-.3,-.2);
         glEnd();
@@ -63,6 +57,9 @@ void Rat::Draw(){
 void Rat::RatView(){
     glEnable(GL_DEPTH_TEST);
     glLoadIdentity();
-    // camera is not good is locked by position need to find out how to get it to rotate with degrees
-    gluLookAt(x, y, .5, x+1, y+1, 0, 0, 0, 1);
+    double z_level = .25;
+    double at_x = x + dx;
+    double at_y = y + dy;
+    double at_z = z_level;
+    gluLookAt(x,y,z_level,  at_x, at_y, at_z,  0,0,1);
 }
