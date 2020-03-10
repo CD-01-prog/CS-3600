371f315e5ad87e1b71439e879e9e18628ff9c85d
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-10 16:31:18 -0600

    Create rat.hpp

---
+++ p7/rat.hpp
@@ -0,0 +1,41 @@
+//
+//  Rat.hpp
+//  Prog5
+//
+//  Created by Caleb J. Delaney on 2/20/20.
+//  Copyright © 2020 Caleb J. Delaney. All rights reserved.
+//
+
+#ifndef Rat_hpp
+#define Rat_hpp
+
+#include <stdio.h>
+class Rat
+{
+public:
+    Rat();
+    double Getx(){return x;};
+    double Gety(){return y;};
+    double Getdegrees(){return degrees;};
+    double Getdx(){return dx;};
+    double Getdy(){return dy;};
+    double GetSpeed(){return Speed;};
+    double Getsize(){return size;};
+    void SpinLeft();
+    void SpinRight();
+    void ScurryForward();
+    void ScurryBackward();
+    void Draw();
+    void RatView();
+    
+private:
+    double x;
+    double y;
+    double degrees;
+    double dx;
+    double dy;
+    double Speed;
+    double size;
+};
+
+#endif /* Rat_hpp */
